f8686a85a279055f9ce0a8ed424d723cb4d3c121
Alvin Lee <alvin.lee@arm.com>
2019-03-12 13:41:49 +0800

    add RSPI in bootloader, modify config for pelion-ready test

--- main.cpp
+++ main.cpp
@@ -22,6 +22,10 @@
 //#include "FATFileSystem.h"
 #include "LittleFileSystem.h"
 
+#if defined(SENSOR_HDC1050)
+#include "hdc1050.h"
+#endif
+
 // An event queue is a very useful structure to debounce information between contexts (e.g. ISR and normal threads)
 // This is great because things such as network operations are illegal in ISR, so updating a resource in a button's fall() function is not allowed
 EventQueue eventQueue;
@@ -35,55 +39,36 @@ StorageHelper sh(bd, &fs);
 NetworkInterface *net = NetworkInterface::get_default_instance();
 
 // Declaring pointers for access to Pelion Device Management Client resources outside of main()
+#if defined(SENSOR_HDC1050)
+MbedCloudClientResource *temperature_res;
+MbedCloudClientResource *humidity_res;
+#endif
+
 MbedCloudClientResource *button_res;
-MbedCloudClientResource *pattern_res;
+
+static uint16_t sim_count = 0;
+static float fTemperature = 0.0f;
+static uint16_t u16Humidity = 0;
 
 // This function gets triggered by the timer. It's easy to replace it by an InterruptIn and fall() mode on a real button
-void fake_button_press() {
-    int v = button_res->get_value_int() + 1;
+void resource_scan() {
 
-    button_res->set_value(v);
+        int v = button_res->get_value_int() + 1;
 
-    printf("Simulated button clicked %d times\n", v);
-}
+        button_res->set_value(v);
 
-/**
- * PUT handler
- * @param resource The resource that triggered the callback
- * @param newValue Updated value for the resource
- */
-void pattern_updated(MbedCloudClientResource *resource, m2m::String newValue) {
-    printf("PUT received, new value: %s\n", newValue.c_str());
-}
+        printf("Simulated button clicked %d times\n", v);	
 
-/**
- * POST handler
- * @param resource The resource that triggered the callback
- * @param buffer If a body was passed to the POST function, this contains the data.
- *               Note that the buffer is deallocated after leaving this function, so copy it if you need it longer.
- * @param size Size of the body
- */
-void blink_callback(MbedCloudClientResource *resource, const uint8_t *buffer, uint16_t size) {
-    printf("POST received. Going to blink LED pattern: %s\n", pattern_res->get_value().c_str());
+#if defined(SENSOR_HDC1050)
 
-    static DigitalOut augmentedLed(LED1); // LED that is used for blinking the pattern
+	HDC1050_GetSensorData(&fTemperature, &u16Humidity);
 
-    // Parse the pattern string, and toggle the LED in that pattern
-    string s = std::string(pattern_res->get_value().c_str());
-    size_t i = 0;
-    size_t pos = s.find(':');
-    while (pos != string::npos) {
-        wait_ms(atoi(s.substr(i, pos - i).c_str()));
-        augmentedLed = !augmentedLed;
+	printf("### Observe_res data update per %ds to keep alive: count: %d, time: %ds, temperature:%.2f, humidity:%d\n", 
+						20, v, 20*v, fTemperature, u16Humidity);
+	temperature_res->set_value(fTemperature);
+	humidity_res->set_value(u16Humidity);
 
-        i = ++pos;
-        pos = s.find(':', pos);
-
-        if (pos == string::npos) {
-            wait_ms(atoi(s.substr(i, s.length()).c_str()));
-            augmentedLed = !augmentedLed;
-        }
-    }
+#endif
 }
 
 /**
@@ -91,8 +76,8 @@ void blink_callback(MbedCloudClientResource *resource, const uint8_t *buffer, ui
  * @param resource The resource that triggered the callback
  * @param status The delivery status of the notification
  */
-void button_callback(MbedCloudClientResource *resource, const NoticationDeliveryStatus status) {
-    printf("Button notification, status %s (%d)\n", MbedCloudClientResource::delivery_status_to_string(status), status);
+void resource_callback(MbedCloudClientResource *resource, const NoticationDeliveryStatus status) {
+    printf("Resource notification, status %s (%d)\n", MbedCloudClientResource::delivery_status_to_string(status), status);
 }
 
 /**
@@ -104,8 +89,20 @@ void registered(const ConnectorClientEndpointInfo *endpoint) {
 }
 
 int main(void) {
+    printf("[FOTA TEST]\n");
     printf("Starting Simple Pelion Device Management Client example\n");
     printf("Connecting to the network...\n");
+
+#if defined(SENSOR_HDC1050)
+	//
+    // Initiation for board
+    //
+    HDC1050_Init();
+
+    fTemperature = 0.0f;
+    u16Humidity = 0;
+
+#endif
 
     // Connect to the internet (DHCP is expected to be on)
     nsapi_error_t status = net->connect();
@@ -130,20 +127,28 @@ int main(void) {
         return -1;
     }
     // Creating resources, which can be written or read from the cloud
+
+#if defined(SENSOR_HDC1050)
+
+    temperature_res = client.create_resource("3303/0/5700", "temperature");
+    temperature_res->set_value(0.0f);
+    temperature_res->methods(M2MMethod::GET);
+    temperature_res->observable(true);
+    temperature_res->attach_notification_callback(resource_callback);
+
+    humidity_res = client.create_resource("3304/0/5700", "humidity");
+    humidity_res->set_value(0);
+    humidity_res->methods(M2MMethod::GET);
+    humidity_res->observable(true);
+    humidity_res->attach_notification_callback(resource_callback);
+
+#endif
+
     button_res = client.create_resource("3200/0/5501", "button_count");
     button_res->set_value(0);
     button_res->methods(M2MMethod::GET);
     button_res->observable(true);
-    button_res->attach_notification_callback(button_callback);
-
-    pattern_res = client.create_resource("3201/0/5853", "blink_pattern");
-    pattern_res->set_value("500:500:500:500:500:500:500:500");
-    pattern_res->methods(M2MMethod::GET | M2MMethod::PUT);
-    pattern_res->attach_put_callback(pattern_updated);
-
-    MbedCloudClientResource *blink_res = client.create_resource("3201/0/5850", "blink_action");
-    blink_res->methods(M2MMethod::POST);
-    blink_res->attach_post_callback(blink_callback);
+    button_res->attach_notification_callback(resource_callback);
 
     printf("Initialized Pelion Client. Registering...\n");
 
@@ -156,7 +161,7 @@ int main(void) {
     // Placeholder for callback to update local resource when GET comes.
     // The timer fires on an interrupt context, but debounces it to the eventqueue, so it's safe to do network operations
     Ticker timer;
-    timer.attach(eventQueue.event(&fake_button_press), 20.0);
+    timer.attach(eventQueue.event(&resource_scan), 20.0);
 
     // You can easily run the eventQueue in a separate thread if required
     eventQueue.dispatch_forever();
